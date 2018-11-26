e1649f80bb0f357be1e69076def6b4f2d0a0a55a
Anurag Kumar <anuragak@users.noreply.github.com>
2018-11-26 01:39:55 +0000

    restored orginial react frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Filter from './components/Filter';
+import Count from './components/Count';
+import Movies from './components/Movies';
+import Limit from './components/Limit';
 
 class App extends Component {
+  constructor(){
+  super();
+  this.state = {
+    movies:       [],
+    showFilter:   false,
+    offset:       40,
+    imdb:         7,
+    year:         '',
+    star_rating:  4,
+    star_count:   50,
+    genre:        undefined
+  };
+
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+  componentDidMount() {
+    this.handleSubmit();
+  };
+
+  handleChange = (key, value) => {
+    this.setState({ [key] : value });
+  }
+
+  toggleFilter = () => {
+    this.setState({ showFilter: !this.state.showFilter });
+  }
+
+  showMore = () => {
+    let query = this.buildQuery();
+    query += `&offset=${this.state.offset}`
+    this.setState({ offset: this.state.offset + 40 })
+    fetch(query)
+      .then(res => res.json())
+      .then(result => this.setState({ movies: this.state.movies.concat(result.rows) }));
+  }
+
+  buildQuery = () => {
+    const params = {
+      imdb:         this.state.imdb,
+      year:         this.state.year,
+      star_rating:  this.state.star_rating,
+      star_count:   this.state.star_count,
+      genre:        this.state.genre,
+    };
+    let query = `/api/movies?`;
+    query += Object.keys(params)
+      .filter(a => typeof params[a] !== 'undefined')
+      .map(k => `${k}=${params[k]}`)
+      .join('&');
+
+      return query;
+  }
+
+  handleSubmit = () => {
+    const query = this.buildQuery();
+    fetch(query)
+      .then(res => res.json())
+      .then(result => this.setState({ movies: result.rows }));
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Amazon Prime Movie Filter</h1>
+        <button className='toggle_filter' onClick={this.toggleFilter}>Filter</button>
+        {this.state.showFilter && <Filter
+          filterMovies = { this.filterMovies }
+          updateParams = { this.handleChange }
+          handleSubmit = { this.handleSubmit }
+          showFilter = { this.state.showFilter }
+          starRating = { this.state.star_rating }
+          starCount = { this.state.star_count }
+          year = { this.state.year }
+          imdb = { this.state.imdb }
+        />}
+        <Count moviesLength = { this.state.movies.length } />
+        <Movies movies = { this.state.movies } />
+        {this.state.movies.length>=40 && <Count moviesLength = { this.state.movies.length } />}
+        {this.state.movies.length>=this.state.offset && <Limit showMore = { this.showMore } />}
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
--- index.js
+++ index.js
@@ -1,64 +1,57 @@
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// const { Client } = require('pg');
-// const { Pool } = require('pg');
+const { Client } = require('pg');
+const { Pool } = require('pg');
 // require('dotenv').config();
 
 var app = express();
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// const pool = new Pool(
-//   {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// }
-// );
-//
-// pool.on('error', (err, client) => {
-//   console.error('Unexpected error on idle client', err)
-//   process.exit(-1)
-// });
-//
-// app.get('/api/movies', function(req, res) {
-//   const params = {
-//     imdb:         req.query.imdb,
-//     year:         req.query.year,
-//     star_rating:  req.query.star_rating,
-//     star_count:   req.query.star_count,
-//     genre:        req.query.genre,
-//   };
-//
-//   let offset = typeof req.query.offset !== 'undefined' ? req.query.offset : 0
-//   let payload = 'SELECT * FROM movies WHERE imdb IS NOT NULL';
-//   let query = Object.keys(params)
-//     .filter(a => typeof params[a] !== 'undefined' && params[a] !== '')
-//     .map(k => k == 'genre' ? `'${params[k]}' = ANY(${k})` :`${k} >= ${params[k]}`)
-//     .join(' AND ');
-//
-//   if (query.length > 0) payload += ` AND ${query}`;
-//   payload += ` ORDER BY imdb DESC LIMIT 40 OFFSET ${offset};`
-//   console.log('payload', payload);
-//   pool.connect()
-//   .then(client => {
-//     return client.query(payload)
-//       .then(result => {
-//         client.release()
-//         res.json(result)
-//       })
-//       .catch(err => {
-//         client.release()
-//         console.log(err.stack)
-//       })
-//   })
-// });
+const pool = new Pool(
+  {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
+);
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+});
+
+app.get('/api/movies', function(req, res) {
+  const params = {
+    imdb:         req.query.imdb,
+    year:         req.query.year,
+    star_rating:  req.query.star_rating,
+    star_count:   req.query.star_count,
+    genre:        req.query.genre,
+  };
+
+  let offset = typeof req.query.offset !== 'undefined' ? req.query.offset : 0
+  let payload = 'SELECT * FROM movies WHERE imdb IS NOT NULL';
+  let query = Object.keys(params)
+    .filter(a => typeof params[a] !== 'undefined' && params[a] !== '')
+    .map(k => k == 'genre' ? `'${params[k]}' = ANY(${k})` :`${k} >= ${params[k]}`)
+    .join(' AND ');
+
+  if (query.length > 0) payload += ` AND ${query}`;
+  payload += ` ORDER BY imdb DESC LIMIT 40 OFFSET ${offset};`
+  console.log('payload', payload);
+  pool.connect()
+  .then(client => {
+    return client.query(payload)
+      .then(result => {
+        client.release()
+        res.json(result)
+      })
+      .catch(err => {
+        client.release()
+        console.log(err.stack)
+      })
+  })
+});
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
