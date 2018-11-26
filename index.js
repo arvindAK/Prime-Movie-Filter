3318a1356208591988d6c9d3df4b5db8be08aeeb
Anurag Kumar <anuragak@users.noreply.github.com>
2018-11-26 01:17:11 +0000

    commented out the database connections

--- index.js
+++ index.js
@@ -14,50 +14,50 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const pool = new Pool(
-  {
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-}
-);
-
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
-});
-
-app.get('/api/movies', function(req, res) {
-  const params = {
-    imdb:         req.query.imdb,
-    year:         req.query.year,
-    star_rating:  req.query.star_rating,
-    star_count:   req.query.star_count,
-    genre:        req.query.genre,
-  };
-
-  let offset = typeof req.query.offset !== 'undefined' ? req.query.offset : 0
-  let payload = 'SELECT * FROM movies WHERE imdb IS NOT NULL';
-  let query = Object.keys(params)
-    .filter(a => typeof params[a] !== 'undefined' && params[a] !== '')
-    .map(k => k == 'genre' ? `'${params[k]}' = ANY(${k})` :`${k} >= ${params[k]}`)
-    .join(' AND ');
-
-  if (query.length > 0) payload += ` AND ${query}`;
-  payload += ` ORDER BY imdb DESC LIMIT 40 OFFSET ${offset};`
-  console.log('payload', payload);
-  pool.connect()
-  .then(client => {
-    return client.query(payload)
-      .then(result => {
-        client.release()
-        res.json(result)
-      })
-      .catch(err => {
-        client.release()
-        console.log(err.stack)
-      })
-  })
-});
+// const pool = new Pool(
+//   {
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true
+// }
+// );
+//
+// pool.on('error', (err, client) => {
+//   console.error('Unexpected error on idle client', err)
+//   process.exit(-1)
+// });
+//
+// app.get('/api/movies', function(req, res) {
+//   const params = {
+//     imdb:         req.query.imdb,
+//     year:         req.query.year,
+//     star_rating:  req.query.star_rating,
+//     star_count:   req.query.star_count,
+//     genre:        req.query.genre,
+//   };
+//
+//   let offset = typeof req.query.offset !== 'undefined' ? req.query.offset : 0
+//   let payload = 'SELECT * FROM movies WHERE imdb IS NOT NULL';
+//   let query = Object.keys(params)
+//     .filter(a => typeof params[a] !== 'undefined' && params[a] !== '')
+//     .map(k => k == 'genre' ? `'${params[k]}' = ANY(${k})` :`${k} >= ${params[k]}`)
+//     .join(' AND ');
+//
+//   if (query.length > 0) payload += ` AND ${query}`;
+//   payload += ` ORDER BY imdb DESC LIMIT 40 OFFSET ${offset};`
+//   console.log('payload', payload);
+//   pool.connect()
+//   .then(client => {
+//     return client.query(payload)
+//       .then(result => {
+//         client.release()
+//         res.json(result)
+//       })
+//       .catch(err => {
+//         client.release()
+//         console.log(err.stack)
+//       })
+//   })
+// });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
