4c676c8f8ddaf11ae819a0d567f345fee4dd7243
Anurag Kumar <anuragak@users.noreply.github.com>
2018-11-26 01:32:22 +0000

    altered possible conflicting express path

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var app = express();
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // const pool = new Pool(
 //   {
